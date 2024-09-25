52f7681b4c99bfeaddaa273ac8b13b4d77bd8d1e
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-25 16:38:41 -0300

    fix: size texts

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -6,16 +6,16 @@ import { SigninForm } from "@/components/auth/signin-form";
 export default function Page() {
   return (
     <div className="max-w-lg mx-auto mt-12 px-6">
-      <Logo size={56} />
-      <h1 className="mt-10 text-2xl">Entre na sua conta</h1>
+      <Logo size={46} />
+      <h1 className="mt-10 text-xl">Entre na sua conta</h1>
       <div className="mt-10 mb-14 flex flex-col gap-6">
         <SigninForm />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-1 md:flex-row">
-        <div className="text-gray-500">Ainda não tem uma conta?</div>
+        <div className="text-gray-600 text-sm">Ainda não tem uma conta?</div>
 
-        <Link href={"/signup"} className="hover:underline">
+        <Link href={"/signup"} className="hover:underline text-sm">
           Cadastre-se
         </Link>
       </div>
