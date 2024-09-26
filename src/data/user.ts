f0d727107466f49caca257ecc6f12ea35c11af41
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-26 17:01:42 -0300

    test: user fake

---
+++ src/data/user.ts
@@ -0,0 +1,13 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+  slug: "mateus",
+  name: "Mateus",
+  lastName: "Alves",
+  avatar: "https://ui-avatars.com/api/?background=random&name=Mateus&size=100",
+  cover:
+    "https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg",
+  bio: "Desenvolvedor júnior há mais de 2 anos",
+  link: "https://www.linkedin.com/in/mateusalvesds/",
+  postCount: 127,
+};
