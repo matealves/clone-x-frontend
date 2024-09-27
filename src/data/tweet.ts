183d9b9496fbed6148959e23a5920b5986eff080
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-27 12:22:22 -0300

    feat: type and data tweet

---
+++ src/data/tweet.ts
@@ -0,0 +1,16 @@
+import { Tweet } from "@/types/tweet";
+import { user } from "./user";
+
+export const tweet: Tweet = {
+  id: 123,
+  user: user,
+  body: "Outro dia Mágico!",
+  image:
+    "https://img.freepik.com/fotos-gratis/retrato-3d-de-pessoas_23-2150793856.jpg",
+  likeCount: 523,
+  commentCount: 61,
+  retweetCount: 0,
+  liked: true,
+  retweeted: false,
+  dataPost: new Date(2024, 8, 1, 10, 0, 0),
+};
