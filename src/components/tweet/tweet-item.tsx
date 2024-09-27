078890ff15e09568766872b23176d46650f802d6
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-27 12:41:16 -0300

    feat: buttons post

--- src/components/tweet/tweet-item.tsx
+++ src/components/tweet/tweet-item.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { Tweet } from "@/types/tweet";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
+import {
+  faRetweet,
+  faHeart as faHeartFilled,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useState } from "react";
 
 type Props = {
   tweet: Tweet;
 };
 
 export const TweetItem = ({ tweet }: Props) => {
+  const [liked, setLiked] = useState(tweet.liked);
+
+  const handleLikeButton = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className="flex gap-2 p-6 border-b-2 border-gray-900">
       <div>
@@ -36,7 +51,38 @@ export const TweetItem = ({ tweet }: Props) => {
           </div>
         )}
 
-        <div className="flex mt-6 text-gray-500">botoes</div>
+        <div className="flex mt-6 text-gray-500">
+          <div className="flex-1">
+            <Link href={`/tweet/${tweet.id}`}>
+              <div className="inline-flex items-center gap-2 cursor-pointer">
+                <FontAwesomeIcon icon={faComment} className="size-4" />
+                <div className="text-sm">{tweet.commentCount}</div>
+              </div>
+            </Link>
+          </div>
+
+          <div className="flex-1">
+            <div className="inline-flex items-center gap-2 cursor-pointer">
+              <FontAwesomeIcon icon={faRetweet} className="size-4" />
+              <div className="text-sm">{tweet.retweetCount}</div>
+            </div>
+          </div>
+
+          <div className="flex-1">
+            <div
+              onClick={handleLikeButton}
+              className={`inline-flex items-center gap-2 cursor-pointer ${
+                liked && "text-red-400"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={liked ? faHeartFilled : faHeart}
+                className="size-4"
+              />
+              <div className="text-sm">{tweet.likeCount}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
