0b08c7f144b69cc3feb6295851cae154a12ce400
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-26 21:40:34 -0300

    feat: home header and menu

---
+++ src/components/home/home-header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Logo } from "../ui/logo";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { HomeMenu } from "./home-menu";
+
+export default function HomeHeader() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div>
+      <header className="flex justify-between p-6 border-b-2 border-gray-900">
+        <div className="lg:hidden">
+          <Logo size={24} />
+        </div>
+        <div className="hidden lg:block text-xl">Página inicial</div>
+
+        <div
+          onClick={() => setShowMenu(true)}
+          className="cursor-pointer lg:hidden"
+        >
+          <FontAwesomeIcon icon={faBars} className="size-6" />
+        </div>
+
+        {showMenu && <HomeMenu closeAction={() => setShowMenu(false)} />}
+      </header>
+    </div>
+  );
+}
