4373d2b175c8dcbbfb1fafa432161defe4e4f765
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-27 17:32:35 -0300

    style: fix border

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -31,7 +31,7 @@ export default function Layout({ children }: Props) {
 
       <section className="flex-1 max-w-lg">{children}</section>
 
-      <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
+      <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-screen w-96 px-8 py-6 border-l-2 border-gray-900">
         <SearchInput hideOnSearch />
         <TreadingArea />
         <RecommendationArea />
