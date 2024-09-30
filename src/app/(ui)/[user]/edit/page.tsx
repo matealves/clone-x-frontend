4394b6752e70ebee1d83957af2552eb4ea354d68
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-30 16:29:11 -0300

    feat: edit profile page

---
+++ src/app/(ui)/[user]/edit/page.tsx
@@ -0,0 +1,73 @@
+import { Button } from "@/components/ui/button";
+import { GeneralHeader } from "@/components/ui/general-header";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { user } from "@/data/user";
+import { faCamera, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Page() {
+  return (
+    <div className="">
+      <GeneralHeader backHref="/profile">
+        <div className="font-bold text-sm">Editar perfil</div>
+      </GeneralHeader>
+
+      <section className="border-b-2 border-gray-900">
+        <div
+          className="flex justify-center items-center gap-4 bg-gray-500 h-28 bg-no-repeat bg-cover bg-center"
+          style={{ backgroundImage: "url(" + user.cover + ")" }}
+        >
+          <div className="cursor-pointer bg-black/80 flex justify-center items-center size-12 rounded-full hover:bg-black">
+            <FontAwesomeIcon icon={faCamera} className="size-6" />
+          </div>
+
+          <div className="cursor-pointer bg-black/80 flex justify-center items-center size-12 rounded-full hover:bg-black">
+            <FontAwesomeIcon icon={faTrash} className="size-5" />
+          </div>
+        </div>
+
+        <div className="-mt-12 px-6">
+          <img
+            src={user.avatar}
+            alt={user.name}
+            className="size-24 rounded-full"
+          />
+          <div className="-mt-24 size-24 flex justify-center items-center">
+            <div className="cursor-pointer bg-black/80 flex justify-center items-center size-12 rounded-full hover:bg-black">
+              <FontAwesomeIcon icon={faCamera} className="size-6" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="p-6 flex flex-col gap-4">
+        <label>
+          <p className="text-md text-gray-500 mb-2">Nome</p>
+          <Input placeholder="Digite seu nome" value={user.name} />
+        </label>
+
+        <label>
+          <p className="text-md text-gray-500 mb-2">Sobrenome</p>
+          <Input placeholder="Digite seu sobrenome" value={user.lastName} />
+        </label>
+
+        <label>
+          <p className="text-md text-gray-500 mb-2">Bio</p>
+          <Textarea
+            placeholder="Descreva você mesmo"
+            rows={4}
+            value={user.bio}
+          />
+        </label>
+
+        <label>
+          <p className="text-md text-gray-500 mb-2">Link</p>
+          <Input placeholder="Digite seu link" value={user.link} />
+        </label>
+
+        <Button label="Salvar alterações" size={1} />
+      </section>
+    </div>
+  );
+}
