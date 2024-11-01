384b75294a525dbf036d643fa44e24416110ac28
Mateus Alves <contatomateusalves@hotmail.com>
2024-10-31 23:13:44 -0300

    fix: user page

--- src/app/(ui)/[user]/page.tsx
+++ src/app/(ui)/[user]/page.tsx
@@ -1,39 +1,54 @@
+"use client";
+
 import { ProfileFeed } from "@/components/profile/profile-feed";
 import { Button } from "@/components/ui/button";
 import { GeneralHeader } from "@/components/ui/general-header";
-import { user } from "@/data/user";
+import { user, user2, user3 } from "@/data/user";
+import { User } from "@/types/user";
+import { tweets } from "@/data/tweet";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Page() {
-  const isMe = true;
+  const pathName = usePathname().replace("/", "");
+  const isMe = pathName === "mateus";
+
+  const users: User[] = [user2, user3];
+  const userClicked = isMe
+    ? user
+    : users.find((user: User) => user.username === pathName)!;
+
+  const tweetsUser = tweets.filter((tweet) => tweet.user.username === pathName);
 
   return (
     <div className="">
       <GeneralHeader backHref="/">
         <div className="font-bold text-sm">
-          {user.name} {user.lastName}
+          {userClicked.name} {userClicked.lastName}
         </div>
-        <div className="text-xs text-gray-500">{user.postCount} posts</div>
+        <div className="text-xs text-gray-500">
+          {userClicked.postCount} posts
+        </div>
       </GeneralHeader>
 
       <section className="border-b-2 border-gray-900">
         <div
           className="bg-gray-500 h-28 bg-no-repeat bg-cover bg-center"
-          style={{ backgroundImage: "url(" + user.cover + ")" }}
+          style={{ backgroundImage: "url(" + userClicked.cover + ")" }}
         ></div>
 
         <div className="-mt-12 flex justify-between items-end px-6">
           <img
-            src={user.avatar}
-            alt={user.name}
+            src={userClicked.avatar}
+            alt={userClicked.name}
             className="size-24 rounded-full"
           />
 
           <div className="w-20">
             {isMe && (
-              <Link href={`/${user.username}/edit`}>
+              <Link href={`/${userClicked.username}/edit`}>
                 <Button label="Editar perfil" size={3} />
               </Link>
             )}
@@ -44,22 +59,22 @@ export default function Page() {
 
         <div className="px-6 mt-4">
           <div className="font-bold text-lg">
-            {user.name} {user.lastName}
+            {userClicked.name} {userClicked.lastName}
           </div>
 
-          <div className="text-sm text-gray-500">@{user.username}</div>
+          <div className="text-sm text-gray-500">@{userClicked.username}</div>
 
-          <div className="py-4 text-gray-500">{user.bio}</div>
+          <div className="py-4 text-gray-500">{userClicked.bio}</div>
 
-          {user.link && (
+          {userClicked.link && (
             <div className="flex gap-2 items-center ">
               <FontAwesomeIcon icon={faLink} className="size-5" />
               <Link
-                href={user.link}
+                href={userClicked.link}
                 target="_blank"
                 className="text-sm text-blue-300"
               >
-                {user.link}
+                {userClicked.link}
               </Link>
             </div>
           )}
@@ -75,7 +90,7 @@ export default function Page() {
         </div>
       </section>
 
-      <ProfileFeed />
+      <ProfileFeed tweets={tweetsUser} />
     </div>
   );
 }
