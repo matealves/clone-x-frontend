6e830538f1a1d957f28a42bb0b8c2f2608da54a8
Mateus Alves <contatomateusalves@hotmail.com>
2024-10-31 23:15:53 -0300

    feat: users fake

--- src/data/user.ts
+++ src/data/user.ts
@@ -6,8 +6,33 @@ export const user: User = {
   username: "mateus",
   avatar: "https://ui-avatars.com/api/?background=random&name=Mateus&size=100",
   cover:
-    "https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg",
-  bio: "Desenvolvedor júnior há mais de 2 anos",
+    "https://media.licdn.com/dms/image/v2/C4E16AQE24FCESiNQ9g/profile-displaybackgroundimage-shrink_200_800/profile-displaybackgroundimage-shrink_200_800/0/1633638372654?e=2147483647&v=beta&t=CW9BbGWwctSAkYzG0beaSNIGsaDFQxn2utYwSdSZF1Q",
+  bio: "Desenvolvedor de Software há mais de 2 anos",
   link: "https://www.linkedin.com/in/mateusalvesds/",
-  postCount: 127,
+  postCount: 2,
+};
+
+export const user2: User = {
+  name: "Luiza",
+  lastName: "Oblak",
+  username: "luizaoblak",
+  avatar:
+    "https://ui-avatars.com/api/?background=random&name=LuizaOblak&size=100",
+  cover:
+    "https://img.freepik.com/free-photo/cityscape-paris-sunlight-blue-sky-fra_181624-50289.jpg?t=st=1730418303~exp=1730421903~hmac=5ae9caab094dec30d49682af4e7d79d763d1cb6c8653d3b2d292b5e5e287f1cc&w=996",
+  bio: "Viajando pelo mundo... ♥",
+  link: "",
+  postCount: 2,
+};
+
+export const user3: User = {
+  name: "Pedro",
+  lastName: "Henrique",
+  username: "ph_10",
+  avatar: "https://ui-avatars.com/api/?background=random&name=Pedro&size=100",
+  cover:
+    "https://chutedeplaca.com.br/wp-content/uploads/2024/08/Noite-da-Copa-Libertadores-Recorde-de-Publico-Esperado-no-Morumbi-no-Duelo-do-Sao-Paulo-scaled.webp",
+  bio: "",
+  link: "",
+  postCount: 2,
 };
